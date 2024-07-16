fa4c2c2f9f751d5d629023be7f26da8e1559ac31
EPSIHYEON <127606365+EPSIHYEON@users.noreply.github.com>
2024-07-17 01:25:19 +0900

    BlackMove 만들고 인트로 완성

--- 나는음식의왕이될꺼야_김밥편_/Assets/김치편scripts/인트로script.cs
+++ 나는음식의왕이될꺼야_김밥편_/Assets/김치편scripts/인트로script.cs
@@ -6,21 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class 인트로script : MonoBehaviour
 {
-    public Text dialogueText;
+    public Text dialogueText1;
+    public Text nextText2;
     public Text nameText;// Reference to your UI Text component
     public GameObject 김밥panel;
+    public GameObject BlackMove;
 
 
     string first = "떄로는 2004년... 음식을 다스리던 대장금께서 돌아가시고 만다.... ";
     string[] dialogues = {  "급작스럽게 돌아가신 대장금께서는\n차기의 후계자를 설정하시지 못하고\n음식의 나라는 혼란에 빠지고 만다. ",
         "그 혼란을 지켜보던 ((((   ))))이는... 혼란을 막고 싶었다  ",
-    "그는.... 혼란을 막고자... ","<size=25><<<<<</size><size=40>왕</size><size=25>>>>>이 되고자 했고, </size>" ,"궁궐인 부엌으로 향하게 된다 "}; // Array of dialogues to display  //꼭 스페이스를 마지막에 눌러주세요
-    string[] namepanel = { "김밥", "김치", "김밥", "김치", "김밥", "김밥", "김치", "김치", "김밥", "김치", "김밥", "김밥", "김치", "김치" };
+    "그는.... 혼란을 막고자... ","<<<<왕>>>>>이 되고자 했고" ,"궁궐인 부엌으로 향하게 된다 "}; // Array of dialogues to display  //꼭 스페이스를 마지막에 눌러주세요
+    string[] dialogues2 = { "그래서 내가 누구냐고? ", "아~ 측면으로 있어서 안보였구나! ", "김밥이야!!! ", "맛과 모양, 가격까지 갖춘 내가 당연히 왕의 될 존재 아니겠어? " };
+    string[] namepanel = { "???", "???", "김밥", "김밥" };
     public Image[] characterImage;
+    public Image Gimbab;
     private bool isTyping = false;
     private int exnumber = 0;
     private int currentDialogueIndex1 = 0;
     private int currentDialogueIndex2 = 0;
+    private int currentDialogue2Index = 0;
 
     int[] imageNumber = { 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1 };
 
@@ -53,8 +58,21 @@ public class 인트로script : MonoBehaviour
 
             else
             {
-              
-                //Invoke("SetScene", 1f);
+                if (currentDialogue2Index < dialogues2.Length)
+                {
+                    dialogueText1.enabled = false;
+                    김밥panel.SetActive(true);
+
+                    showName();
+                    ShowDialogue2();
+                    StartCoroutine(typing2(dialogues2[currentDialogue2Index - 1]));
+                }
+
+
+                else {
+                    BlackMove.SetActive(true);
+                    Invoke("SetScene", 1f);
+                }
             }
         }
     }
@@ -67,7 +85,7 @@ public class 인트로script : MonoBehaviour
 
 
         // Update the text component with the current dialogue
-        dialogueText.text = currentDialogue;
+        dialogueText1.text = currentDialogue;
 
 
         // Move to the next dialogue in the array
@@ -82,13 +100,29 @@ public class 인트로script : MonoBehaviour
         // Get the next dialogue from the array
         string currentDialogue = dialogues[currentDialogueIndex2];
 
-
+       
         // Update the text component with the current dialogue
-        dialogueText.text = currentDialogue;
+        dialogueText1.text = currentDialogue;
 
 
         // Move to the next dialogue in the array
         currentDialogueIndex2++;
+        Debug.Log("문자");
+    }
+
+    void ShowDialogue2() {
+
+        string currentDialogue = dialogues2[currentDialogue2Index];
+
+        if (currentDialogue == dialogues2[2]) {
+            Gimbab.gameObject.SetActive(true);
+        }
+        // Update the text component with the current dialogue
+        nextText2.text = currentDialogue;
+
+
+        // Move to the next dialogue in the array
+        currentDialogue2Index++;
         Debug.Log("문자");
     }
     void showName()
@@ -123,8 +157,11 @@ public class 인트로script : MonoBehaviour
         isTyping = true;
         for (int i = 0; i <= currentDialogue.Length; i++)
         {
+          
+                
+            
 
-            dialogueText.text = currentDialogue.Substring(0, i); // 현재 인덱스까지의 문자열을 표시
+            dialogueText1.text = currentDialogue.Substring(0, i); // 현재 인덱스까지의 문자열을 표시
 
             yield return new WaitForSeconds(0.05f); // 0.05초마다 한 글자씩 표시
 
@@ -135,6 +172,28 @@ public class 인트로script : MonoBehaviour
 
 
     }
+
+    IEnumerator typing2(string currentDialogue)
+    {
+        isTyping = true;
+        for (int i = 0; i <= currentDialogue.Length; i++)
+        {
+
+
+
+
+            nextText2.text = currentDialogue.Substring(0, i); // 현재 인덱스까지의 문자열을 표시
+
+            yield return new WaitForSeconds(0.05f); // 0.05초마다 한 글자씩 표시
+
+
+        }
+        isTyping = false;
+
+
+
+    }
+
 
     void firstshowName()
     {
@@ -170,7 +229,7 @@ public class 인트로script : MonoBehaviour
         for (int i = 0; i <= firstDialogue.Length; i++)
         {
 
-            dialogueText.text = firstDialogue.Substring(0, i); // 현재 인덱스까지의 문자열을 표시
+            dialogueText1.text = firstDialogue.Substring(0, i); // 현재 인덱스까지의 문자열을 표시
 
             yield return new WaitForSeconds(0.05f); // 0.05초마다 한 글자씩 표시
 
@@ -182,4 +241,8 @@ public class 인트로script : MonoBehaviour
 
     }
 
+    void SetScene()
+    {
+        SceneManager.LoadScene("김치1");
+    }
 }
