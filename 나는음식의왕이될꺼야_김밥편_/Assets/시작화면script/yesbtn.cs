b8924fdb1229bc41ee63fefcd60dc278b9111ed9
EPSIHYEON <127606365+EPSIHYEON@users.noreply.github.com>
2024-07-21 01:42:45 +0900

    인트로 완료, 시작화면 완료

---
+++ 나는음식의왕이될꺼야_김밥편_/Assets/시작화면script/yesbtn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yesbtn : MonoBehaviour
+{
+    public GameObject Musicpanel;
+
+public void yesBtn() {
+        Musicpanel.SetActive(false);
+    
+    }
+}
