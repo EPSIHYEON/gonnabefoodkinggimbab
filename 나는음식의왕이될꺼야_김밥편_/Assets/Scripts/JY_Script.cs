fcb38c2b66dd76eabdb94824ff217fc4550c1da5
Sunho12 <rhkrtjsgh1210@gmail.com>
2024-07-26 16:49:54 +0900

    :sparkles: [Feat] 스테이지 2,3 Scene1 파일 수정
    script에 첫 대사는 자동으로 나머지 대사는 클릭했을 시 나오도록 설정

--- 나는음식의왕이될꺼야_김밥편_/Assets/Scripts/JY_Script.cs
+++ 나는음식의왕이될꺼야_김밥편_/Assets/Scripts/JY_Script.cs
@@ -6,81 +6,74 @@ using UnityEngine.SceneManagement;
 
 public class JY_Script : MonoBehaviour
 {
-    public Text dialogueText;
     public Text nameText;// Reference to your UI Text component
-    string[] dialogues = { "허억.. 허억.. 집 안인가.... 이제 곧.. 부엌에 도달할 수 있는건가..? ", "으디서 피도 안 마른 것이 왕이 되려혀!!!!!!!!!!!!!!! ", "아, 아닛 스승님!!! ",
+
+    string first = "허억.. 허억.. 집 안인가.... 이제 곧.. 부엌에 도달할 수 있는건가..? ";
+    string[] dialogues = { "으디서 피도 안 마른 것이 왕이 되려혀!!!!!!!!!!!!!!! ", "아, 아닛 스승님!!! ",
     "그래 이놈아, 겨우겨우 인간 놈들에게 먹히라고 햄 끼워넣어줬더니 뭐??? 왕?????? ", "스승님, 예전에 야채맛 가득 나는 제가 아닙니다!! 제 당근도 이제 볶.은.당.근 이라구요!!!! ",
     "볶.은.당.근??? 난 태생부터 볶는 제육인디 어딜 덤비려 혀!!!!!!!!!!! ", "크흑...! 스승님... 결국... 저는 스승님을 이겨야 나아갈 수 있는 겁니까!! ", "나의 살갖이 다 타버리기 전까진 절대 보내줄수 없는 기야!!!!!!!!! 덤벼라!!!! "}; // Array of dialogues to display  //꼭 스페이스를 마지막에 눌러주세요
-    string[] namepanel = { "김밥", "?????" , "김밥", "제육", "김밥", "제육", "김밥", "제육"};
+    string[] namepanel = { "?????" , "김밥", "제육", "김밥", "제육", "김밥", "제육"};
     public Image[] characterImage;
+    public bool isTyping = false;
     private int exnumber = 0;
     private int currentDialogueIndex1 = 0;
     private int currentDialogueIndex2 = 0;
-    int[] imageNumber = { 0, 1, 0, 1, 0, 1, 0, 1};
+    int[] imageNumber = { 1, 0, 1, 0, 1, 0, 1};
  
     public GameObject BlackOut;
+    public GameObject BlackMove;
+    public Text dialogueText;
+    public AudioSource scriptSound;
     private Coroutine typingCoroutine;
-    private bool isTyping = false;
+
+    void Start()
+    {
+        BlackMove.SetActive(true);
+        firstshowImage();
+        firstshowName();
+        firstshowDialogue();
+        StartCoroutine(firsttyping(first));
+    }
 
     void Update()
     {
         // Check if the mouse button is clicked
-        if (Input.GetMouseButtonDown(0))
+        if ((Input.GetKeyDown("space") && !isTyping) || Input.GetMouseButtonDown(0) && !isTyping)
         {
-            if (isTyping)
-            {   
-                // Skip to the end of the current dialogue
-                StopCoroutine(typingCoroutine);
-                ShowNextDialogue();
-                isTyping = false;
-            }
-            else
-            {
-                // Check if there are more dialogues to display
-                if (currentDialogueIndex2 < dialogues.Length)
-                {
-                    ShowNextDialogue();
-                }
-                else
-                {
-                    BlackOut.SetActive(true);
-                    Invoke("SetScene", 1f);
-                }
-            }
-        }
-            /*
+            firstshowImage(false);
+                
             // Check if there are more dialogues to display
             if (currentDialogueIndex2 < dialogues.Length)
             {
                 showImage();
                 showName();
                 ShowDialogue();
-                typingCoroutine = StartCoroutine(_typing(dialogues[currentDialogueIndex2 -1]));
-                // Display the next dialogue
-
-
-                }
-
+                typingCoroutine = StartCoroutine(typing(dialogues[currentDialogueIndex2 - 1]));
+            }
+                
             else
             {
                 BlackOut.SetActive(true);
                 Invoke("SetScene", 1f);
             }
-        }*/
-    }
-
-    void ShowNextDialogue()
-    {
-        if (typingCoroutine != null)
-        {
-            StopCoroutine(typingCoroutine);
         }
-        showImage();
-        showName();
+    }
+    
+    
 
+    void ShowDialogue()
+    {
+        // Get the next dialogue from the array
         string currentDialogue = dialogues[currentDialogueIndex2];
-        typingCoroutine = StartCoroutine(_typing(currentDialogue));
+
+
+        // Update the text component with the current dialogue
+        dialogueText.text = currentDialogue;
+
+
+        // Move to the next dialogue in the array
         currentDialogueIndex2++;
+        Debug.Log("문자");
     }
 
     
@@ -107,36 +100,73 @@ public class JY_Script : MonoBehaviour
 
     }
 
-    IEnumerator _typing(string currentDialogue)
+    IEnumerator typing(string currentDialogue)
     {
         isTyping = true;
-        dialogueText.text = "";
-        for (int i = 0; i < currentDialogue.Length; i++)
+        
+
+        for (int i = 0; i <= currentDialogue.Length; i++)
         {
-            dialogueText.text = currentDialogue.Substring(0, i+1);
-            yield return new WaitForSeconds(0.15f);
+            dialogueText.text = currentDialogue.Substring(0, i);
+
+            yield return new WaitForSeconds(0.05f);
         }
         isTyping = false;
     }
 
 
-/*
-    IEnumerator _typing(string currentDialogue)
+    void firstshowDialogue()
     {
-         for (int i = 0; i < currentDialogue.Length; i++)
-            {
+        string currentDialogue = first;
 
-                dialogueText.text = currentDialogue.Substring(0, i);
+        dialogueText.text = currentDialogue;
+    }
+
+    void firstshowName()
+    {
+        string currentname = "김밥";
+
+        nameText.text = currentname;
+
+    }
+
+    void firstshowImage(bool active = true)
+    {
 
 
-            yield return new WaitForSeconds(0.15f);
 
-
-            }
+        if (active)
+        {
+            characterImage[0].gameObject.SetActive(true);
+        }
+        else
+        {
+            characterImage[0].gameObject.SetActive(false);
+        }
 
 
     }
-*/
+
+
+    IEnumerator firsttyping(string firstDialogue)
+    {
+        isTyping = true;
+        for (int i = 0; i <= firstDialogue.Length; i++)
+        {
+            
+            dialogueText.text = firstDialogue.Substring(0, i); // 현재 인덱스까지의 문자열을 표시
+            //scriptSound.Play();
+            yield return new WaitForSeconds(0.05f); // 0.05초마다 한 글자씩 표시
+           
+
+        }
+        isTyping = false;
+
+
+
+    }
+
+
     void SetScene()
     {
         SceneManager.LoadScene("제육2");
--- 나는음식의왕이될꺼야_김밥편_/Assets/Scripts/PA_Script.cs
+++ 나는음식의왕이될꺼야_김밥편_/Assets/Scripts/PA_Script.cs
@@ -6,37 +6,49 @@ using UnityEngine.SceneManagement;
 
 public class PA_Script : MonoBehaviour
 {
-    public Text dialogueText;
+
     public Text nameText;// Reference to your UI Text component
-    string[] dialogues = {"드디어!! 부엌인가!! 드디어..! 내가 왕이 되.. ", "꺄악 왕자님!!! ", "뭐...뭐지??? 저... 해괴망측한 음식은??? ","까아ㅏ악!! 파스타 왕자님!!!! ", "(뭐??? 파스타??? 대장금께서도... 한번도 알려주신 적 없는 음식인데???) ", "(설마... 양(洋)의 것인가?? 어째서 왕의 자리인 저 자리에???) ", "어째서 거기 있을 수 있는거냐!!!! ", "One's desires are insatiable ", "뭐..뭣..?? 뭐래는 거냐!!!! ", "'사람의 욕망은 만족할 줄 모른다.' 영어도 모르는거냐. 우매하군. ", "사람은 음식에게 어떤 것을 가장 높이 평가한다고 생각하는가? ", "그건... 당연히... 합리적인 가격이 아니겠어?? ", "그럼.. 나는 합리적인 가격인 것인가? ", "(보자... 가격이... 말도 안돼!! 16000원?? 저딴 게 어떻게 왕에 자리에???) ", "꺄아ㅏ악! 왕자님!!! ", "(잔치국수는 4500원에도 먹는데, 저딴 면쪼까리랑 뭐가 다르다고 저 가격에 열광하는 거지??) ", "가장 높은 가치는..... ", "분위기다. ","만족할 줄 모르는 인간은 이제 맛과 모양과 가격보다.. 분위기를 따지게 된 것이다. ", "거짓말치지 마!! 대장금께서 음식은 모든 인간을 보살피는 존재랬어!! 너같은 가격은 그저 상류층을 위한 것일 뿐이야! ", "뭐..? 분위기라면...! 음식을 먹는 그 자체로 행복한! 그게 분위... ", "우매하군 우매해. ", "음식은 이제... '먹기만' 해서는 안돼... ", "뭐???? 무슨 소리지??? ", "가르쳐주지.. 덤벼라. "}; // Array of dialogues to display  //꼭 스페이스를 마지막에 눌러주세요
-    string[] namepanel = { "김밥", "?????", "김밥", "?????", "김밥", "김밥", "김밥", "파스타", "김밥", "파스타", "파스타", "김밥", "파스타", "김밥", "?????", "김밥", "파스타", "파스타", "파스타", "김밥",  "파스타", "김밥", "파스타", "파스타", "김밥", "파스타"};
+    string first = "드디어!! 부엌인가!! 드디어..! 내가 왕이 되.. ";
+    string[] dialogues = {"꺄악 왕자님!!! ", "뭐...뭐지??? 저... 해괴망측한 음식은??? ","까아ㅏ악!! 파스타 왕자님!!!! ", "(뭐??? 파스타??? 대장금께서도... 한번도 알려주신 적 없는 음식인데???) ", "(설마... 양(洋)의 것인가?? 어째서 왕의 자리인 저 자리에???) ", "어째서 거기 있을 수 있는거냐!!!! ", "One's desires are insatiable ", "뭐..뭣..?? 뭐래는 거냐!!!! ", "'사람의 욕망은 만족할 줄 모른다.' 영어도 모르는거냐. 우매하군. ", "사람은 음식에게 어떤 것을 가장 높이 평가한다고 생각하는가? ", "그건... 당연히... 합리적인 가격이 아니겠어?? ", "그럼.. 나는 합리적인 가격인 것인가? ", "(보자... 가격이... 말도 안돼!! 16000원?? 저딴 게 어떻게 왕에 자리에???) ", "꺄아ㅏ악! 왕자님!!! ", "(잔치국수는 4500원에도 먹는데, 저딴 면쪼까리랑 뭐가 다르다고 저 가격에 열광하는 거지??) ", "가장 높은 가치는..... ", "분위기다. ","만족할 줄 모르는 인간은 이제 맛과 모양과 가격보다.. 분위기를 따지게 된 것이다. ", "거짓말치지 마!! 대장금께서 음식은 모든 인간을 보살피는 존재랬어!! 너같은 가격은 그저 상류층을 위한 것일 뿐이야! ", "뭐..? 분위기라면...! 음식을 먹는 그 자체로 행복한! 그게 분위... ", "우매하군 우매해. ", "음식은 이제... '먹기만' 해서는 안돼... ", "뭐???? 무슨 소리지??? ", "가르쳐주지.. 덤벼라. "}; // Array of dialogues to display  //꼭 스페이스를 마지막에 눌러주세요
+    string[] namepanel = {"?????", "김밥", "?????", "김밥", "김밥", "김밥", "파스타", "김밥", "파스타", "파스타", "김밥", "파스타", "김밥", "?????", "김밥", "파스타", "파스타", "파스타", "김밥",  "파스타", "김밥", "파스타", "파스타", "김밥", "파스타"};
     public Image[] characterImage;
+    public bool isTyping = false;
     private int exnumber = 0;
     private int currentDialogueIndex1 = 0;
     private int currentDialogueIndex2 = 0;
-    int[] imageNumber = { 0, 2, 0, 2, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 2, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1};
+    int[] imageNumber = { 2, 0, 2, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 2, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1};
  
     public GameObject BlackOut;
+    public GameObject BlackMove;
+    public Text dialogueText;
+    public AudioSource scriptSound;
     private Coroutine typingCoroutine;
 
+    void Start()
+    {
+        BlackMove.SetActive(true);
+        firstshowImage();
+        firstshowName();
+        firstshowDialogue();
+        StartCoroutine(firsttyping(first));
+    }
+    
     void Update()
     {
         // Check if the mouse button is clicked
-        if (Input.GetMouseButtonDown(0))
+        if ((Input.GetKeyDown("space") && !isTyping) || Input.GetMouseButtonDown(0) && !isTyping)
         {
-
+            firstshowImage(false);
+                
             // Check if there are more dialogues to display
             if (currentDialogueIndex2 < dialogues.Length)
             {
                 showImage();
                 showName();
                 ShowDialogue();
-                typingCoroutine = StartCoroutine(_typing(dialogues[currentDialogueIndex2 -1]));
-                // Display the next dialogue
-
-
-                }
-
+                typingCoroutine = StartCoroutine(typing(dialogues[currentDialogueIndex2 - 1]));
+            }
+                
             else
             {
                 BlackOut.SetActive(true);
@@ -84,24 +96,72 @@ void showName() {
 
 
 
-    IEnumerator _typing(string currentDialogue)
+    IEnumerator typing(string currentDialogue)
     {
-         for (int i = 0; i < currentDialogue.Length; i++)
-            {
+        isTyping = true;
+        
 
-                dialogueText.text = currentDialogue.Substring(0, i);
+        for (int i = 0; i <= currentDialogue.Length; i++)
+        {
+            dialogueText.text = currentDialogue.Substring(0, i);
+
+            yield return new WaitForSeconds(0.05f);
+        }
+        isTyping = false;
+    }
+
+    void firstshowDialogue()
+    {
+        string currentDialogue = first;
+
+        dialogueText.text = currentDialogue;
+    }
+
+    void firstshowName()
+    {
+        string currentname = "김밥";
+
+        nameText.text = currentname;
+
+    }
+
+    void firstshowImage(bool active = true)
+    {
 
 
-            yield return new WaitForSeconds(0.15f);
+
+        if (active)
+        {
+            characterImage[0].gameObject.SetActive(true);
+        }
+        else
+        {
+            characterImage[0].gameObject.SetActive(false);
+        }
 
 
-            }
+    }
+
+    IEnumerator firsttyping(string firstDialogue)
+    {
+        isTyping = true;
+        for (int i = 0; i <= firstDialogue.Length; i++)
+        {
+            
+            dialogueText.text = firstDialogue.Substring(0, i); // 현재 인덱스까지의 문자열을 표시
+            //scriptSound.Play();
+            yield return new WaitForSeconds(0.05f); // 0.05초마다 한 글자씩 표시
+           
+
+        }
+        isTyping = false;
+
 
 
     }
 
     void SetScene()
     {
-        SceneManager.LoadScene("제육2");
+        SceneManager.LoadScene("파스타2");
     }
 }
