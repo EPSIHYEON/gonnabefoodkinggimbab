1d7a1d2be98ee684e810ca49f6db684b2986896d
EPSIHYEON <127606365+EPSIHYEON@users.noreply.github.com>
2024-07-26 18:53:08 +0900

    linerenderer로 한번 만들어봄

--- 나는음식의왕이될꺼야_김밥편_/Assets/pasta_script/laserSpawner.cs
+++ 나는음식의왕이될꺼야_김밥편_/Assets/pasta_script/laserSpawner.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class bulletspawner : MonoBehaviour
+public class laserSpawner : MonoBehaviour
 {
-  
     public GameObject bulletPrefab; // 총알 프리팹
+    public GameObject laser;
     public Transform[] spawnPoints; // 8개의 위치를 저장할 배열
     public float spawnInterval = 1f; // 총알이 생성되는 간격
     public float speed = 10f;
@@ -17,10 +17,9 @@ public class bulletspawner : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
 
-
         StartCoroutine(SpawnBullets());
 
-      
+
     }
 
     IEnumerator SpawnBullets()
@@ -45,18 +44,22 @@ public class bulletspawner : MonoBehaviour
         Transform spawnPoint = spawnPoints[randomIndex];
 
         GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
-        
+        //GameObject laserr = Instantiate(laser, spawnPoint.position, spawnPoint.rotation);
+
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        //Rigidbody2D rb2 = laserr.GetComponent<Rigidbody2D>();
 
         if (rb != null)
         {
             if (randomIndex == 0 || randomIndex == 1 || randomIndex == 2)
             {
                 rb.velocity = new Vector2(0, -1).normalized * speed;
+                
             }
             else if (randomIndex == 3 || randomIndex == 4)
             {
                 rb.velocity = new Vector2(5, -3).normalized * speed;
+
             }
             // 나머지 경우에 대한 속도 설정 추가 가능
             else if (randomIndex == 5 || randomIndex == 6)
@@ -65,7 +68,4 @@ public class bulletspawner : MonoBehaviour
             }
         }
     }
-
 }
-
-    
