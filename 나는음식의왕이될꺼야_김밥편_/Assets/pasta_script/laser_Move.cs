988f3615010c7c833702346989ebb829c6e1f7c4
EPSIHYEON <127606365+EPSIHYEON@users.noreply.github.com>
2024-07-31 00:07:17 +0900

    불투명도 조절

--- 나는음식의왕이될꺼야_김밥편_/Assets/pasta_script/laser_Move.cs
+++ 나는음식의왕이될꺼야_김밥편_/Assets/pasta_script/laser_Move.cs
@@ -9,6 +9,8 @@ public class laser_Move : MonoBehaviour
     public Vector3 fixedEndPosition; // 레이저의 고정된 끝의 위치
     public float laserWidth = 1f; // 레이저의 두께
     public LineRenderer lineRenderer; // 레이저의 LineRenderer 컴포넌트
+    public Color lineColor = new Color(1f, 1f, 1f, 0.5f); // 흰색, 불투명도 0.5
+
 
 
 
@@ -16,12 +18,15 @@ public class laser_Move : MonoBehaviour
     {
         // LineRenderer 컴포넌트를 가져오거나 추가합니다.
         lineRenderer = GetComponent<LineRenderer>();
+       
 
         if (lineRenderer == null)
         {
             Debug.LogError("LineRenderer가 Beam에 추가되지 않았습니다.");
             return;
         }
+        lineRenderer.startColor = lineColor;
+        lineRenderer.endColor = lineColor;
 
         // LineRenderer의 속성을 설정합니다.
         lineRenderer.startWidth = laserWidth;
