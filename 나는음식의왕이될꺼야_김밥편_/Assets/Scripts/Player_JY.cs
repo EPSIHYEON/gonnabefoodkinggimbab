f5d571ae36ca8918137a1514ddd32075d0a44e93
Sunho12 <rhkrtjsgh1210@gmail.com>
2024-08-21 15:51:52 +0900

    :truck: JY2 Scene 새로 생성

--- 나는음식의왕이될꺼야_김밥편_/Assets/Scripts/Player_JY.cs
+++ 나는음식의왕이될꺼야_김밥편_/Assets/Scripts/Player_JY.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Player_JY : MonoBehaviour
+public class player_JY : MonoBehaviour
 {
     public AudioSource diesound;
     public int speed;
@@ -13,20 +13,20 @@ public class Player_JY : MonoBehaviour
     public Button restartButton;
     public Image[] lifes;
     private int i = 0;
-
-    Rigidbody2D rigid;
-    Vector3 lastValidPosition;
-
+   
+     Rigidbody2D rigid;
     void Start()
     {
-        restartButton.gameObject.SetActive(false);
         rigid = GetComponent<Rigidbody2D>();
         active = true;
-        lastValidPosition = transform.position;
+        bullets.SetActive(true);
     }
 
+    // Update is called once per frame
     void Update()
     {
+
+
         if (active == true)
         {
             move();
@@ -36,61 +36,71 @@ public class Player_JY : MonoBehaviour
                 shoot();
             }
         }
+
     }
 
     void move()
     {
+
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         Vector3 move = new Vector3(h, v, 0);
-        Vector3 newPosition = move * Time.deltaTime * speed + transform.position;
-
-        // 이동 시도 전에 마지막으로 유효했던 위치 저장
-        lastValidPosition = transform.position;
-        transform.position = newPosition;
+        transform.position = move * Time.deltaTime * speed + transform.position;
     }
 
     void shoot()
     {
         Instantiate(bullets, transform.position, transform.rotation);
-        Debug.Log("발사되고 있음");
+        Debug.Log("player shoot");
     }
 
-    public void Restart()
+
+
+    public void Restart_K()
     {
         Debug.Log("재시작");
 
-        SceneManager.LoadScene("JY2");
+        SceneManager.LoadScene("JY2-1");
 
         active = true;
     }
 
+    public void Restart_P()
+    {
+        Debug.Log("재시작");
+
+        SceneManager.LoadScene("JY2-1");
+
+        active = true;
+
+
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // "INBorder" 태그와 충돌했을 때
-        if (collision.gameObject.tag == "INBorder" && collision.gameObject.layer == LayerMask.NameToLayer("BorderIn"))
-        {
-            Debug.Log("경계에 부딪힘: 되돌아갑니다.");
-            transform.position = lastValidPosition; // 플레이어를 마지막 유효 위치로 되돌림
-        }
-
-        // 예: 다른 충돌 처리
         if (collision.gameObject.tag == "jbullet")
         {
             if (i < lifes.Length)
             {
+                Destroy(collision.gameObject);
                 Destroy(lifes[i]);
                 diesound.Play();
                 i++;
+
             }
             else
             {
                 diesound.Play();
                 active = false;
-                gameObject.SetActive(false);
                 restartButton.gameObject.SetActive(true);
+
+
             }
+
+
+
+
         }
     }
 }
-
